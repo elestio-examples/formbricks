703ba047d6ac11b258abff8f73ec846796038362
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2024-01-26 15:14:08 +0530

    Update preInstall.sh for smtp

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -4,9 +4,39 @@ set -o allexport; source .env; set +o allexport;
 NEXTAUTH_SECRET=$(openssl rand -base64 32)
 ENCRYPTION_KEY=$(openssl rand -hex 32)
 
+cat /opt/elestio/startPostfix.sh > post.txt
+filename="./post.txt"
+
+SMTP_LOGIN=""
+SMTP_PASSWORD=""
+
+# Read the file line by line
+while IFS= read -r line; do
+  # Extract the values after the flags (-e)
+  values=$(echo "$line" | grep -o '\-e [^ ]*' | sed 's/-e //')
+
+  # Loop through each value and store in respective variables
+  while IFS= read -r value; do
+    if [[ $value == RELAYHOST_USERNAME=* ]]; then
+      SMTP_LOGIN=${value#*=}
+    elif [[ $value == RELAYHOST_PASSWORD=* ]]; then
+      SMTP_PASSWORD=${value#*=}
+    fi
+  done <<< "$values"
+
+done < "$filename"
+
+rm post.txt
+
 cat << EOT >> ./.env
 ENCRYPTION_KEY=${ENCRYPTION_KEY}
 NEXTAUTH_SECRET=${NEXTAUTH_SECRET}
+MAIL_FROM=${SMTP_LOGIN}
+SMTP_HOST=tuesday.mxrouting.net
+SMTP_PORT=465
+SMTP_SECURE_ENABLED=1
+SMTP_USER=${SMTP_LOGIN}
+SMTP_PASSWORD=${SMTP_PASSWORD}
 EOT
 
 cat <<EOT > ./servers.json
